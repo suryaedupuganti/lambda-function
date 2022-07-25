d9c41d3a6e28aea649bf5b70ffe55cdc9adb6028
Surya Vara Prasad Edupuganti <surya_edupuganti@persistent.com>
2022-07-25 20:53:04 +0530

    modified tf files

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,5 +2,6 @@ module "lambda_deployer" {
     source = "./modules/lambda_deployment"
 
     deployment_bucket_name = var.deployment_bucket_name
+    deployment_path_key = var.deployment_path_key
     deployment_package_name = var.deployment_package_name
 }
