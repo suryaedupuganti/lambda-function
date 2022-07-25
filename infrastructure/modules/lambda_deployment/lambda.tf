0800c42fd7653e0a9f2b4a6c9452e684731e57be
Surya Vara Prasad Edupuganti <surya_edupuganti@persistent.com>
2022-07-25 21:16:05 +0530

    modified tf files

--- infrastructure/modules/lambda_deployment/lambda.tf
+++ infrastructure/modules/lambda_deployment/lambda.tf
@@ -21,9 +21,11 @@ EOF
 resource "aws_lambda_function" "test_lambda" {
   function_name = "my-sample-lambda"
   s3_bucket        = var.deployment_bucket_name
-  s3_key = filemd5("${var.deployment_path_key}/${var.deployment_package_name}.zip")
+  s3_key = "${var.deployment_path_key}/${var.deployment_package_name}.zip"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = filebase64sha256("${var.deployment_path_key}/${var.deployment_package_name}.zip")
 
   runtime = "python3.8"
 }
