9df688d5cd36c881bdac26aa937e0e4aaacb58b7
Surya Vara Prasad Edupuganti <surya_edupuganti@persistent.com>
2022-07-25 20:12:36 +0530

    modified tf files

--- infrastructure/modules/lambda_deployment/lambda.tf
+++ infrastructure/modules/lambda_deployment/lambda.tf
@@ -1,15 +1,14 @@
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
 
-  assume_role_policy = filemd5("iam/lambda-assume-policy.json")
+  assume_role_policy = filemd5("./iam/lambda-assume-policy.json")
 }
 
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
   role = aws_iam_role.lambda_role.id
 
-  
-  policy = filemd5("iam/lambda-policy.json")
+  policy = filemd5("./iam/lambda-policy.json")
 }
 
 resource "aws_lambda_function" "test_lambda" {
