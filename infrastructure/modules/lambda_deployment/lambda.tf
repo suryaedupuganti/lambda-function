c96d0378cfe32e8ee0a472955c6acc94c1d57638
Surya Vara Prasad Edupuganti <surya_edupuganti@persistent.com>
2022-07-25 20:41:08 +0530

    modified tf files

--- infrastructure/modules/lambda_deployment/lambda.tf
+++ infrastructure/modules/lambda_deployment/lambda.tf
@@ -1,21 +1,28 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
-  assume_role_policy = filemd5("./../iam/lambda-assume-policy.json")
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
-
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda_policy"
-  role = aws_iam_role.lambda_role.id
-
-  policy = filemd5("./../iam/lambda-policy.json")
+EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "${var.deployment_path_key}/${var.deployment_package_name}.zip"
   s3_bucket        = var.deployment_bucket_name
-  function_name = "welcome.py"
-  role          = aws_iam_role.lambda_role.arn
+  s3_key = "${var.deployment_path_key}/${var.deployment_package_name}.zip"
+  function_name = "my-sample-lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "welcome.hello"
 
   runtime = "python3.8"
