12452e7842b0d3be05bdc6b86dc1bb5b7e8c77d1
Surya Vara Prasad Edupuganti <surya_edupuganti@persistent.com>
2022-07-25 23:10:01 +0530

    modified lambda.tf

--- infrastructure/modules/lambda_deployment/lambda.tf
+++ infrastructure/modules/lambda_deployment/lambda.tf
@@ -30,7 +30,7 @@ resource "aws_lambda_function" "test_lambda" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
 
-  source_code_hash = filebase64sha256("${var.deployment_path_key}/${var.deployment_package_name}.zip")
+  # source_code_hash = filebase64sha256("${var.deployment_path_key}/${var.deployment_package_name}.zip")
 
   runtime = "python3.8"
 }
